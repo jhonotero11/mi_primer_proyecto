515fc7e45096edd7ac2e1db913ba6576d0902c67
John Otero <john.otero90@unisalamanca.edu.co>
2025-11-12 15:15:33 -0500

    Primerr commit: script de saludo en python

---
+++ hola.py
@@ -0,0 +1,6 @@
+#hola.py
+#Autor John Otero
+#Descripción: Saludo básico al usuario con mi código en python.
+
+nombre = input("¿cómo te llamas?")
+print(f"¡Hola, {nombre}! Bienvenido a tu primer proyecto en Git Hub y Python")
